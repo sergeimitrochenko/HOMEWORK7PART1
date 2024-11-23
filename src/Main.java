2a47d81d9169df05e40bdfd0da6ae984a0e97fb9
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-11-23 14:27:15 +0300

    "Задача № 1"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Задача №1");
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        for (
+                int i : weight) {
+            if (i < weight.length)
+                System.out.print(i + ",");
+            else {
+                System.out.print(i);
+            }
+        }
     }
 }
+
