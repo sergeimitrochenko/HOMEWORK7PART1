da6b0002de2e948f75979d628ef5b04408f2b6fb
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-11-23 14:48:53 +0300

    "Задача № 1,№2,№3"

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,34 @@ public class Main {
         for (
                 int i : weight) {
             if (i < weight.length)
-    System.out.print(i + ",");
+                System.out.print(i + ",");
             else {
-System.out.print(i);
+                System.out.print(i);
             }
         }
         System.out.println("Задача № 2");
-
-        double[] arr = {1.57,7.654,9.986};
+        double[] arr = {1.57, 7.654, 9.986};
         for (
                 int i = 0;
                 i < arr.length; i++) {
             if (i < arr.length - 1)
+                System.out.print(arr[i] + ", ");
+            else {
+                System.out.print(arr[i]);
+            }
+        }
+        System.out.println("Задача № 3");
+        for (int i = weight.length - 1; i >= 0; i--) {
+            if (i != 0)
+                System.out.print(weight[i] + ", ");
+            else {
+                System.out.print(weight[i]);
+            }
+        }
+
+        System.out.println("Задача № 3");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i != 0)
                 System.out.print(arr[i] + ", ");
             else {
                 System.out.print(arr[i]);
